e005e20eab747288c607989c561ab4b259dca25c
Ure8925 <suzzylicious24@gmail.com>
2024-02-06 16:04:13 +0100

    Added the search functionality using the Api

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
-function handleSearchSubmit(event) {
-event.preventDefault()
-let searchInput=document.querySelector("#search-input");
-let weatherCityElement=document.querySelector("#weather-city");
-weatherCityElement.innerHTML=searchInput.value;
+function displayWeatherInfo(response) {
+  let weatherTemperature = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+   let weatherCityElement = document.querySelector("#weather-city");
+    let description = document.querySelector("#description");
+    description.innerHTML=response.data.condition;
+  
+  
+
+    weatherCityElement.innerHTML = response.data.city;
+  weatherTemperature.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "302ea8odf48556tb65f5340850929f02";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherInfo);
 }
 
-let searchFormElement =document.querySelector("#search-form");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Lagos");
